b0ceb2ea2b481b10354cb32f699cf32111ec922f
lucakrueger <lucakr@outlook.de>
2022-06-08 14:05:49 +0200

    added hash.h

---
+++ hash.h
@@ -0,0 +1,27 @@
+#ifndef BASE_HASH_H__
+#define BASE_HASH_H__
+
+unsigned long djb2(unsigned char *str){
+    unsigned long hash = 5381;
+    int c;
+
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    return hash;
+}
+
+static unsigned long sdbm(unsigned char *str) {
+    unsigned long hash = 0;
+    int c;
+
+    while (c = *str++)
+        hash = c + (hash << 6) + (hash << 16) - hash;
+
+    return hash;
+}
+
+
+
+
+#endif // BASE_HASH_H__
